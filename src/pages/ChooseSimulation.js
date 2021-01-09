25e27519a2cf4a2ea4021d30c21f9486fbf07ea6
dawidplesniarski <dawidplesniarski@gmail.com>
2021-01-09 14:30:54 +0100

    React router config

---
+++ src/pages/ChooseSimulation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {withRouter} from "react-router";
+
+const ChooseSimulation = () => {
+    return(
+        <>
+            <span>Test</span>
+        </>
+    );
+};
+
+export default withRouter(ChooseSimulation);
